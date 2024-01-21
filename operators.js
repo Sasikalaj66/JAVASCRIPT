858da30efd8134f6d99ca3ccf943fad5f291c381
sasi <sasijagan03@email.com>
2024-01-21 19:53:34 +0530

    printing the values using operators

---
+++ operators.js
@@ -0,0 +1,25 @@
+//Assignment Operator
+var a = 10
+console.log("Assignment Operator Output:",a)
+
+//Arithmetic Operator
+var aA=10
+var bA=10
+console.log(typeof aA)  // For check the data type
+console.log("Arithmetic Operator Addition Output:",aA+bA)
+console.log("Arithmetic Operator Substraction Output:",aA-bA)
+console.log("Arithmetic Operator Multiplication Output:",aA*bA)
+console.log("Arithmetic Operator Division Output:",aA/bA)
+console.log("Arithmetic Operator Modulus Output:",aA%bA)
+console.log("Arithmetic Operator Output:",aA==bA)
+console.log("Arithmetic Operator Output:",aA=!bA)
+
+//Logical Operator -> && , || , ! => Output =
+var aL=10
+var bL=20
+console.log("Logical Operator && Output:",aL&&bL)
+console.log("Logical Operator || Output:",aL||bL)
+console.log("Logical Operator ! Output:",!aL)
+
+
+
