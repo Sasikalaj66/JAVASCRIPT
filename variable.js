aaf39e2b0c7b3cb83459e0c6f280828c5cd91474
sasi <sasijagan03@email.com>
2024-01-21 19:52:07 +0530

    print the variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+a="188"
+console.log("1." ,a)
+
+var b="sasi"
+console.log("This is belongs to var type",b)
+b="sasikala"
+console.log("This is belongs to var type",b)
+let c="jagan"
+console.log("This is belongs to let type",c)
+const d="185"
+console.log("This is belongs to const type and it's value is:",d)
