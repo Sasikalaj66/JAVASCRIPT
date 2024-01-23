b24d7c827684b47ed0cad9eae639981c46f50569
sasi <sasijagan03@email>
2024-01-23 16:02:05 +0530

    third commit

--- objects.js
+++ objects.js
@@ -32,13 +32,19 @@ console.log(kongu.food) // dot(.) is used to accessing the key path inside the o
 console.log(kongu["Transport"])  //To access we should know the key
 */
 //for in eg =>output=key
-for(key in kongu){
+/*for(key in kongu){
     console.log(key,kongu[key])
 }
 for(value of kongu){
     console.log(value)
-}
+}*/
+kongu=Object.entries(kongu)
+console.log(kongu)
 //for each eg
-kongu.forEach((key,element)=>{
-    console.log(key,element)
+kongu.forEach(([key,element])=>{
+    console.log(key,":",element)
 })
+//for of =>Here destructuring is used
+for([key,value] of kongu){
+    console.log(key)
+}
