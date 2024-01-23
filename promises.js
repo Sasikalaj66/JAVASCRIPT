66e60b85a22ff72e06bc8ebb18a8fd8a7f617819
sasi <sasijagan03@email>
2024-01-23 12:02:23 +0530

    sec commit

--- promises.js
+++ promises.js
@@ -1,26 +1,26 @@
 //PROMISES
 //Ex : 1
 function isAuthenticated() {
-    return Math.random() > 1;   // not success if symbol is < then it will be success
-  }
-  function Login() {
-    return new Promise(function (response, noresponse) {
-      setTimeout(function () {
-        if (isAuthenticated()) {
-          response("Your Authentication is Successful");
-        } else {
-          noresponse("Your Authentication is not Successful");
-        }
-      }, 1000);
-    });
-  }
-  Login()
-    .then(function (message) {
-      console.log(message);
-    })
-    .catch(function (message) {
-      console.log(message);
-    });
+  return Math.random() > 1;   // not success if symbol is < then it will be success
+}
+function Login() {
+  return new Promise(function (response, noresponse) {
+    setTimeout(function () {
+      if (isAuthenticated()) {
+        response("Your Authentication is Successful");
+      } else {
+        noresponse("Your Authentication is not Successful");
+      }
+    }, 1000);
+  });
+}
+Login()
+  .then(function (message) {
+    console.log(message);
+  })
+  .catch(function (message) {
+    console.log(message);
+  });
 
 //Ex : 2
 function email_id() {
@@ -43,4 +43,39 @@ sasi()
   })
   .catch(function (message) {
     console.log(message);
+  });
+
+//Ex : 3 => Location Implementation
+
+function isLocationValid(Loc) {
+  var v = "Melbourne"
+  if (v === "valid") {
+    return true
+  }
+  else {
+    return false
+  }
+}
+function location() {
+  return new Promise(function(response, errorMsg){
+    setTimeout(function () {
+      Loc = "Melbourne"
+      console.log("The location is",Loc)
+      if(isLocationValid) {
+        return response("Location Found")
+      }
+      else {
+        return errorMsg("Location not Found ")
+      }
+    },2000)
+
+})
+
+}
+location()
+  .then(function(response) {
+      console.log(response);
+  })
+  .catch(function(response){
+    console.log(response);
   });
